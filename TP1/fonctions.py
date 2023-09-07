a2646aa07ffb437ac135190c469438bf54f45dce
U45842209 <marc.tahon.dujardin.bis@gmail.com>
2023-09-07 16:41:26 +0200

    tests

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,6 +3,12 @@ def puissance(a, b):
         pow = 1
         for x in range(b):
             pow = pow*a
+        if a > 0:
+            return pow
+        elif a == 0:
+            return 0
+        else:
+            return f"-{pow}"
         return pow
     except TypeError:
         raise TypeError("Only integers are allowed")
--- TP1/test.py
+++ TP1/test.py
@@ -10,3 +10,10 @@ def test_2():
 
 def test_3 ():
     assert f.puissance(2 ,3) > 0
+    assert f.puissance(2, 2) == 4
+    assert f.puissance(-2, 2) == -4
+    assert f.puissance(2, 0) == 1
+    assert f.puissance(-2, 0) == -1
+    assert f.puissance(0, 0) == 1
+    assert f.puissance(0, 3) == 0
+    assert f.puissance(0, -3) == 0
