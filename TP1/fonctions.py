d42e4bfca81f969e302a2cb839c69adedf6173ff
U45842209 <marc.tahon.dujardin.bis@gmail.com>
2023-09-27 15:32:02 +0200

    changes in fonctions for pytest

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,10 +1,10 @@
 import pytest
 
 def puissance(a, b):
+    if b == 0:
+        raise ZeroDivisionError("L'exposant ne peut pas être zéro.")
     try:
         result = int(a) ** int(b)
         return result
     except ValueError:
         raise ValueError("Les valeurs de la base et de l'exposant doivent être des nombres entier.")
-    except ZeroDivisionError:
-        raise ValueError("L'exposant ne peut pas être zéro.")
