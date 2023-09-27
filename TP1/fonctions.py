1c0ac2ab540adeb7a42879d400665601a8559e61
U45842209 <marc.tahon.dujardin.bis@gmail.com>
2023-09-27 15:52:34 +0200

    all things

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,6 +3,8 @@ import pytest
 def puissance(a, b):
     if b == 0:
         raise ZeroDivisionError("L'exposant ne peut pas être zéro.")
+    if b < 0:
+        raise ValueError("La valeur de l'exposant doit être un nombre entier positif.")
     try:
         result = int(a) ** int(b)
         return result
--- TP1/test.py
+++ TP1/test.py
@@ -11,6 +11,7 @@ def test_2():
 def test_puissance_exception():
     with pytest.raises(ValueError):
         f.puissance(2, "trois")
+        f.puissance(a=2,b=-3)
 
 def test_puissance_exception_2():
     with pytest.raises(ZeroDivisionError):
