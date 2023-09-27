d0e5ae279c91d80468adbc03b816017b41ebc0ba
U45842209 <marc.tahon.dujardin.bis@gmail.com>
2023-09-27 16:03:36 +0200

    all things 4

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -2,15 +2,12 @@ import pytest
 
 def puissance(a, b):
     a, b = int(a), int(b)
-    if b == int:
-        if b < 0:
-            raise ValueError("La valeur de l'exposant doit être un nombre entier positif.")
-        if b == 0:
-            raise ZeroDivisionError("L'exposant ne peut pas être zéro.")
-    if b != int:
-        raise ValueError("La valeur de l'exposant doit être un nombre.")
+    if not isinstance(b, int):
+        raise ValueError("La valeur de l'exposant doit être un nombre entier.")
+    if b <= 0:
+        raise ValueError("L'exposant doit être un nombre entier positif.")
     try:
         result = int(a) ** int(b)
         return result
     except ValueError:
-        raise ValueError("Les valeurs de la base et de l'exposant doivent être des nombres entier.")
+        raise ValueError("Les valeurs de la base et de l'exposant doivent être des nombres entiers.")
