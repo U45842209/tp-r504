da0526d7a35e42bb088bdba362fd932fd60c5578
U45842209 <marc.tahon.dujardin.bis@gmail.com>
2023-09-27 15:38:25 +0200

    changes in fonctions for pytest

--- TP1/test.py
+++ TP1/test.py
@@ -1,5 +1,6 @@
 import fonctions as f
 import pytest
+import sys
 
 def test_1 ():
     assert f.puissance(2 ,3) == 8
@@ -15,3 +16,7 @@ def test_puissance_exception():
 def test_puissance_exception_2():
     with pytest.raises(ZeroDivisionError):
         f.puissance(2, 0)
+
+def test_py_version():
+    python_version = sys.version.split()[0]  # Get the first part of sys.version
+    print("Python Version:", python_version)
